59600b71422663362d97c270f3b86af7a693a99e
tbiskner <tbiskner@debian.ccie.org>
2020-03-03 14:16:40 -0500

    this is the 3rd commit for newBranch

--- testGit.py
+++ testGit.py
@@ -1,7 +1,8 @@
 # a test file for Github
 # this is the 2nd commit for newBranch
-
+# new line, and the 3rd commit for newBranch
 print('test file v1')
 print('added a new branch')
+print('this is the 3rd commit')
 
 
